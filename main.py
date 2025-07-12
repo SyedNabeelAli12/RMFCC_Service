4d416b6cd1d42f28a92cd6ad8df867fc7dedac4b
Nabeel Ali <78920962+SyedNabeelAli12@users.noreply.github.com>
2025-07-12 19:21:10 +0200

    dealing with cors

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("URL")],  # ⚠️ Allows any origin
+    allow_origins=os.getenv("URL"),  # ⚠️ Allows any origin
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
